a30db25c9f85e75a281443c4f2ce8a0bb9cced6d
Thiago Hickmann <thiago.hickmann@ironmountain.com.br>
2018-10-31 19:30:25 -0300

    primeira animação

--- app.js
+++ app.js
@@ -4,9 +4,32 @@ const canvas = document.getElementById('my-canvas');
 // Getting the 2d context.
 const ctx = canvas.getContext('2d');
 
-// Drawing the little rectangle.
-ctx.fillStyle = 'grey';
-ctx.fillRect(230, 230, 50, 50);
+// Our looper control.
+let looper;
 
-ctx.fillStyle = 'blue'
-ctx.fillRect(50,50, 200, 200)
+// Out X value that will be incremented.
+let rectX = 0;
+
+// Stops the animation.
+const stopLooper = () => clearInterval(looper);
+
+// Function that draws the rectangle.
+const drawRectangle = (x) => {
+  // Stops the animation when the rectangle reaches the end.
+  if (x > canvas.width - 50) {
+    stopLooper();
+  }
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(x, 0, 50, 50);
+};
+
+// Each loop we call render function.
+const render = () => {
+  // We clean everything in the canvas.
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Call the function that draws the rectangle incrementing x.
+  drawRectangle(rectX += 1);
+}
+
+// Starting looper.
+looper = setInterval(render, 10);
