604e43ce0af066b232be202b380be0c12a6839f5
Thiago Hickmann <thiago.hickmann@ironmountain.com.br>
2018-10-31 19:17:58 -0300

    renderizando alguma coisa

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const canvas = document.getElementById('my-canvas');
 
 // Getting the 2d context.
 const ctx = canvas.getContext('2d');
+
+// Drawing the little rectangle.
+ctx.fillStyle = 'grey';
+ctx.fillRect(230, 230, 50, 50);
+
+ctx.fillStyle = 'blue'
+ctx.fillRect(50,50, 200, 200)
